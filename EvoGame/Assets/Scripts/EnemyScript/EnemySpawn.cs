37564d08fe6b477c25184a667de9c2109e425f1c
RostiPrkl <atte.ekdahl@gmail.com>
2023-12-11 09:56:50 +0200

    Enemy Update 001
    Initial Enemy Commit, Jellyfish

---
+++ EvoGame/Assets/Scripts/EnemyScript/EnemySpawn.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    
+    [SerializeField] GameObject enemy;
+    [SerializeField] Vector2 spawnArea;
+    [SerializeField] float spawnTimer;
+    [SerializeField] GameObject player;
+    public float timer;
+
+
+    private void Awake()
+    {
+        timer = spawnTimer;
+    }
+
+
+    private void Update()
+
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+            SpawnEnemy();
+    }
+
+
+    private void SpawnEnemy()
+
+    {
+        Vector3 position = GenerateRandomPosition();
+        position += player.transform.position;
+        timer = spawnTimer;
+
+        GameObject newEnemy = Instantiate(enemy);
+        newEnemy.transform.position = position;
+        newEnemy.GetComponent<Enemy_>().SetTarget(player);
+        Enemy_ newEnemyComponent = newEnemy.GetComponent<Enemy_>();
+        newEnemyComponent.SetTarget(player);        
+        newEnemy.transform.parent = transform;
+    }
+
+    private Vector3 GenerateRandomPosition()
+    {
+
+        Vector3 position = new Vector3();
+
+        float f = UnityEngine.Random.value > 0.5f ? -1f : 1f;
+        if (UnityEngine.Random.value > 0.5f)
+        {
+            position.x = UnityEngine.Random.Range(-spawnArea.x, spawnArea.x);
+            position.y = spawnArea.y * f;
+        }
+        else
+        {
+            position.y = UnityEngine.Random.Range(-spawnArea.y, spawnArea.y);
+            position.x = spawnArea.x * f;
+        }
+
+        position.z = 0;
+        return position;
+
+    }
+}
