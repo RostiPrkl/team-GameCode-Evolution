0c49e4d92513e9df0bbb45d0f8cfeeaf4401033d
Riku H <Riku.Hyvarinen@outlook.com>
2023-12-14 11:49:12 +0200

    Evogame

---
+++ Evogameui/EvoGame/Assets/Scripts/DropRateManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropRateManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Drops
+    {
+        public string name;
+        public GameObject itemPrefab;
+        public float dropRate;
+    }
+
+    public List<Drops> drops;
+
+    void OnDestroy()
+    {
+        float randomNumber = Random.Range(0f, 100f);
+        List<Drops> possibleDrops = new();
+
+        foreach (Drops rate in drops)
+        {
+            if(randomNumber <= rate.dropRate)
+                possibleDrops.Add(rate);
+        }
+        if (possibleDrops.Count > 0)
+        {
+            Drops drops = possibleDrops[Random.Range(0, possibleDrops.Count)];
+            Instantiate(drops.itemPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
